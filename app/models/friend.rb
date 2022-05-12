8a0ab37077b10306725c2679252b7d5b5a5b175e
Noah Reece <noah.w.reece@gmail.com>
2022-05-12 16:22:37 -0400

    added dependent destroy back in

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,14 +1,14 @@
 class Friend < ApplicationRecord
   has_many :friendships, dependent: :destroy
   has_many :users, through: :friendships
-  has_many :interactions
+  has_many :interactions, dependent: :destroy
 
   # i do not feel that there is a need for the second
   # has many users being that that relatioship
   # is already created above. not to mention the possible conflict
   # within active record
   # has_many :users, through: :interactions
-  has_many :group_friends
+  has_many :group_friends, dependent: :destroy
   has_many :groups, through: :group_friends
 
   def most_recent_communication_date
